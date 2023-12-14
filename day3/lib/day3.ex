5116438f5a3f2da6d5accfe3dbeab056bad48cb5
Jake Abed <jakeabed@gmail.com>
2023-12-14 08:05:15 -0800

    Clean functions and fix styling

---
+++ day3/lib/day3.ex
@@ -0,0 +1,23 @@
+# 
+
+defmodule Day3 do
+  defp grab_input do
+    {:ok, input} = File.read("lib/input.txt")
+    input
+  end
+
+  defp format_input(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(fn line -> String.graphemes(line)
+      |> Enum.with_index(fn elmnt, idx -> {idx, {elmnt, idx}} end)
+      |> Map.new()
+      end)
+    |> Enum.with_index(fn elmnt, idx -> {idx, elmnt} end)
+    |> Map.new()
+  end
+
+  def printInput() do
+    grab_input() |> format_input()
+  end
+end
