00a30b44b32e89253a76991d3b002470996653b8
Jake Abed <jakeabed@gmail.com>
2023-12-16 11:35:08 -0800

    Format input for day4, come up with idea

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -1,18 +1,17 @@
 defmodule Day4 do
-  @moduledoc """
-  Documentation for `Day4`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day4.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  #Grab input from file and split lines.
+  def input do
+    {:ok, i} = File.read("lib/input.tx")
+    String.trim_trailing(i)
+    |> String.split("\n")
+    |> Enum.map(fn string -> String.split(string, ": ") end)
+    |> IO.inspect()
+    |> Enum.map(fn [_game, ints] -> ints end)
+    |> Enum.map(&String.split(&1, " | "))
+    |> Enum.map(fn [winners, nums] ->
+      [String.split(winners), String.split(nums)] end)
   end
+
+  
+  
 end
