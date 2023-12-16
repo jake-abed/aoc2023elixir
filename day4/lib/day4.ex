11c1678178a96cf82c8517ffd468b229c7a1c41f
Jake Abed <jakeabed@gmail.com>
2023-12-16 12:10:31 -0800

    Finish day4 part 1

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -12,6 +12,26 @@ defmodule Day4 do
       [String.split(winners), String.split(nums)] end)
   end
 
-  
+  def filter_guesses(both_sets) do
+    [winners, guesses] = both_sets
+    Enum.filter(guesses, fn guess -> Enum.any?(winners, fn winner -> guess === winner end) end)
+  end
+
+  defp calc_points(count) do
+    case count do
+      0 -> 0
+      1 -> 1
+      2 -> 2
+      _ -> Integer.pow(2, count - 1)
+    end
+  end
+
+  def get_answer do
+    Day4.input 
+    |> Enum.map(&Day4.filter_guesses/1)
+    |> Enum.map(fn entry -> length(entry) end)
+    |> Enum.map(&calc_points(&1))
+    |> Enum.reduce(fn x, acc -> x + acc end)
+  end
   
 end
